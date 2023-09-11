f6b0b346580d1f9f6d676c275dca6492ff865c76
MarioGuTe <mariogutierreztello@gmail.com>
2023-09-11 17:58:21 -0300

    add Routes and atoms

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Header from "./atoms/Header/Header";
+import Songs from "./molecules/Songs/Songs";
+import Lyrics from "./molecules/Lyrics/Lyrics";
+import NotFound from "./molecules/NotFound/NotFound";
+import "./assets/css/style.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Songs />} />
+        <Route path="/lyrics" element={<Lyrics />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
