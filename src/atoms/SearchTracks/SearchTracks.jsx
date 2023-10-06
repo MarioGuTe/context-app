b56ac553eba804c29b208fcf2397dae1c6921bfe
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-05 23:45:27 -0300

    add SearchTracks logic

--- src/atoms/SearchTracks/SearchTracks.jsx
+++ src/atoms/SearchTracks/SearchTracks.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 
-const SearchTracks = () => {
-  return <div>This is SearchTracks</div>;
-};
+const SearchTracks = ({ validateQTrack }) => (
+  <Paper className="paper defaultPaper">
+    <TextField
+      id="q_track"
+      label="Track"
+      margin="normal"
+      variant="outlined"
+      onKeyUp={(e) => {
+        if (e.key === "Enter") {
+          validateQTrack(e);
+        }
+      }}
+    />
+    <IconButton onClick={(e) => validateQTrack(e)}>
+      <SearchIcon />
+    </IconButton>
+  </Paper>
+);
 
 export default SearchTracks;
--- src/molecules/Songs/Songs.jsx
+++ src/molecules/Songs/Songs.jsx
@@ -1,5 +1,18 @@
+import { useContext } from "react";
+import SearchTracks from "../../atoms/SearchTracks/SearchTracks";
+import Tracks from "../../atoms/Tracks/Tracks";
+import Message from "../../atoms/Message/Message";
+import ProgressBar from "../../atoms/ProgressBar/ProgressBar";
+import { SongsContext } from "../../contexts/Songs/SongsContext";
+
 const Songs = () => {
-  return <div>Thius is the Songs Page</div>;
+  const { validateQTrack, doneFetch, tracks, text } = useContext(SongsContext);
+
+  return (
+    <>
+      <SearchTracks validateQTrack={validateQTrack} />
+    </>
+  );
 };
 
 export default Songs;
